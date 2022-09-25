29bddddf82e38768a0164bc2447a11dc9d63f763
akky-xxxx <114.s.aki@gmail.com>
2022-09-25 18:03:10 +0900

    chore(eslint-config-strict): build@2022-09-25 18:03:10

--- packages/eslint-config-strict/out/next/index.js
+++ packages/eslint-config-strict/out/next/index.js
@@ -1,12 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+const path_1 = __importDefault(require("path"));
 module.exports = {
-    plugins: [
-        "@typescript-eslint",
-        "jest",
-        "storybook",
-        "tsdoc",
-        "unicorn",
-    ],
+    plugins: ["@typescript-eslint", "jest", "storybook", "tsdoc", "unicorn"],
     extends: [
         "prettier",
         "eslint:recommended",
@@ -23,15 +21,17 @@ module.exports = {
         "airbnb",
         "airbnb/hooks",
         "next/core-web-vitals",
-        "../shared/config/import",
-        "../shared/config/jest",
-        "../shared/config/storybook",
-        "../shared/config/tsdoc",
-        "../shared/config/jsx-a11y",
-        "../shared/config/react",
-        "../shared/config/unicorn",
-        "../shared/config/javascript",
-        "../shared/config/typescript",
+        ...[
+            "../shared/config/import",
+            "../shared/config/jest",
+            "../shared/config/storybook",
+            "../shared/config/tsdoc",
+            "../shared/config/jsx-a11y",
+            "../shared/config/react",
+            "../shared/config/unicorn",
+            "../shared/config/javascript",
+            "../shared/config/typescript",
+        ].map((configPath) => path_1.default.resolve(__dirname, configPath)),
     ],
 };
 //# sourceMappingURL=index.js.map
