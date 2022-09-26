abffd5643b82f0be092f3132ac53508480d4827c
akky-xxxx <114.s.aki@gmail.com>
2022-09-26 23:13:38 +0900

    test(eslint-config-strict-check): add dry run code for jsx-no-leaked-render

--- packages/eslint-config-strict-check/dry-run/react/index.tsx
+++ packages/eslint-config-strict-check/dry-run/react/index.tsx
@@ -1,20 +1,34 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Fragment } from "react"
 
-import type { FC } from "react"
+import type { FC, ReactNode } from "react"
+
+type ReactTest1Props = {
+  count: number
+  empty: ReactNode
+  title: string
+}
 
 /**
  * @remarks
  * for checking apply if eslint-plugin-react
  */
 
-export const ReactTest1: FC = () => (
-  // eslint-disable-next-line react/jsx-fragments
-  <>
-    <div>1</div>
-    <div>2</div>
-  </>
-)
+export const ReactTest1: FC<ReactTest1Props> = (props) => {
+  const { count, empty, title } = props
+  return (
+    // eslint-disable-next-line react/jsx-fragments
+    <>
+      <div>1</div>
+      <div>2</div>
+      {/* eslint-disable-next-line react/jsx-no-leaked-render */}
+      <div>{count && title}</div>
+      <div>{Boolean(count) && title}</div>
+      <div>{count ? title : null}</div>
+      <div>{count ? title : empty}</div>
+    </>
+  )
+}
 
 // eslint-disable-next-line react/function-component-definition
 export function ReactTest2() {
