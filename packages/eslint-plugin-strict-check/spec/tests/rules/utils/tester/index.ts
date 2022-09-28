ae42cc5ee7bd48f1e7f8d6e8f28e34cc8c860584
akky-xxxx <114.s.aki@gmail.com>
2022-09-28 23:23:20 +0900

    test(eslint-plugin-strict-check): add property for react into tester

--- packages/eslint-plugin-strict-check/spec/tests/rules/utils/tester/index.ts
+++ packages/eslint-plugin-strict-check/spec/tests/rules/utils/tester/index.ts
@@ -2,4 +2,9 @@ import { RuleTester } from "@typescript-eslint/utils/dist/ts-eslint"
 
 export const tester = new RuleTester({
   parser: require.resolve("@typescript-eslint/parser"),
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
 })
