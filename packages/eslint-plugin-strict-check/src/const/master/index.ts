680118993c25158622aa14b5df7006bf64423ad5
akky-xxxx <114.s.aki@gmail.com>
2022-10-02 01:27:34 +0900

    feat(eslint-plugin-strict-check): register forbidden-hard-coding-href to plguin body

--- packages/eslint-plugin-strict-check/src/const/master/index.ts
+++ packages/eslint-plugin-strict-check/src/const/master/index.ts
@@ -1,5 +1,6 @@
 import { avoidRiskyInputType } from "../../rules/avoid-risky-input-type"
 import { eventHandlerPrefix } from "../../rules/event-handler-prefix"
+import { forbiddenHardCodingHref } from "../../rules/forbidden-hard-coding-href"
 import { Warn } from "../../shared/const"
 
 import type { MasterRecord } from "../../shared/types"
@@ -14,5 +15,10 @@ export const Master: MasterRecord[] = [
     "avoidRiskyInputType",
     avoidRiskyInputType,
     [Warn, { riskyValues: ["email", "number", "tel"] }],
+  ],
+  [
+    "forbiddenHardCodingHref",
+    forbiddenHardCodingHref,
+    [],
   ],
 ]
