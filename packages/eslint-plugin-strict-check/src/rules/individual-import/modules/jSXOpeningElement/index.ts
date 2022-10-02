8347a552760190a09c5253548e43ddd91503e898
akky-xxxx <114.s.aki@gmail.com>
2022-10-02 17:19:25 +0900

    feat(eslint-plugin-strict-check): implement individual-import

---
+++ packages/eslint-plugin-strict-check/src/rules/individual-import/modules/jSXOpeningElement/index.ts
@@ -0,0 +1,44 @@
+import { getErrorMessage } from "../getErrorMessage"
+
+import type { MessageIdList, Option } from "../../types"
+import type {
+  RuleContext,
+  RuleFunction,
+} from "@typescript-eslint/utils/dist/ts-eslint/Rule"
+import type { TSESTree } from "@typescript-eslint/utils/dist/ts-estree"
+
+export type Context = Readonly<RuleContext<MessageIdList, readonly Option[]>>
+type JSXOpeningElement = (
+  context: Context,
+) => RuleFunction<TSESTree.JSXOpeningElement>
+
+export const jsxOpeningElement: JSXOpeningElement = (context) => (node) => {
+  const { options, report } = context
+
+  if (!options.length) {
+    report({
+      messageId: "NoOption",
+      node,
+    })
+    return
+  }
+
+  if (
+    node.name.type !== "JSXMemberExpression" ||
+    node.name.object.type !== "JSXIdentifier"
+  ) {
+    return
+  }
+
+  const {
+    name: {
+      object: { name: moduleName },
+      property: { name: propertyName },
+    },
+  } = node
+
+  report({
+    message: getErrorMessage(moduleName, propertyName),
+    node,
+  })
+}
