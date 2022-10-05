24e6bf32677771d21a33a45be660f2e151eac0fb
akky-xxxx <114.s.aki@gmail.com>
2022-10-05 23:11:07 +0900

    chore(eslint-plugin-strict-check): remove compiled files

--- packages/eslint-plugin-strict-check/out/rules/individual-import/modules/getErrorMessage/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getErrorMessage = void 0;
-const getErrorMessage = (moduleName, propertyName) => `import "${moduleName}.${propertyName}" as individually. example: import { ${propertyName} } from "${moduleName.toLowerCase()}"`;
-exports.getErrorMessage = getErrorMessage;
-//# sourceMappingURL=index.js.map
--- packages/eslint-plugin-strict-check/out/shared/const/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Prefix = exports.Error = exports.Warn = exports.Off = void 0;
-exports.Off = 0;
-exports.Warn = 1;
-exports.Error = 2;
-exports.Prefix = "strict-check";
-//# sourceMappingURL=index.js.map
