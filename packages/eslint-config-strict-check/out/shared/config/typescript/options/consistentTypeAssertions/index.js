a140b670a17bbd8618cb2af5ea29d58c3e27984a
akky-xxxx <114.s.aki@gmail.com>
2022-10-05 22:29:15 +0900

    chore(eslint-config-strict-check): remove compiled files

--- packages/eslint-config-strict-check/out/shared/config/typescript/options/consistentTypeAssertions/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.consistentTypeAssertions = void 0;
-const const_1 = require("../../../../const");
-exports.consistentTypeAssertions = [const_1.Error, { assertionStyle: "never" }];
-//# sourceMappingURL=index.js.map
--- packages/eslint-config-strict-check/out/shared/config/typescript/options/noConfusingVoidExpression/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.noConfusingVoidExpression = void 0;
-const const_1 = require("../../../../const");
-exports.noConfusingVoidExpression = [const_1.Error, { ignoreVoidOperator: true }];
-//# sourceMappingURL=index.js.map
